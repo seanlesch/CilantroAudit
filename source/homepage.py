79ef659203332febbc417e24b623c3864f23ef04
Steven Tran <transtev@pdx.edu>
2019-10-10 13:42:29 -0700

    created a home page

---
+++ source/homepage.py
@@ -0,0 +1,19 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.config import Config
+
+import kivy
+kivy.require('1.11.1')
+
+Config.set('graphics', 'resizable', '0')
+homePageLayout = Builder.load_file('./widgets/homepage.kv')
+
+class HomePage(App):
+
+    def build(self):
+        self.title = 'CilantroAudit'
+        return homePageLayout
+
+
+if __name__ == '__main__':
+    HomePage().run()
