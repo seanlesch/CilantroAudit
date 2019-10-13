fdefc3e02569088acdb2d415b3656d4ece52d190
Erik Nordin <erik@pop-os.localdomain>
2019-10-12 21:06:15 -0700

    Add AuditTemplate Class

--- source/audit_template.py
+++ source/audit_template.py
@@ -1,21 +1,7 @@
 # Header something something something
-
 from mongoengine import *
-
-# db.myCollection.insert({"name": "john", "age" : 22, "location": "colombo"})
-
-
-class Question(Document):
-  questionText = StringField(required = True, max_length = 50)
-  #yesSeverity =
-  #noSeverity =
-  #otherSeverity =
-  #otherText =
-
+from source.question import Question
 
 class AuditTemplate(Document):
-  title = StringField(required = True, max_length = 50)
-  questions = ListField(ReferenceField(Question), required = True)
-
-
-class AuditTemplateBuilder:
+    title = StringField(required=True, max_length=50, min_length=1)
+    questions = EmbeddedDocumentListField(Question, required=True)
