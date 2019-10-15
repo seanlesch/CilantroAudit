952770c355412d4c0fbf8e201c0aef43bf921521
bakolisty <bakolisty@gmail.com>
2019-10-15 14:04:52 -0700

    Adding the work that has gotten me close

--- source/create_audit_page.py
+++ source/create_audit_page.py
@@ -5,7 +5,6 @@ from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
 from kivy.properties import ObjectProperty
-from kivy.properties import ListProperty
 
 kivy.require("1.11.1")
 
@@ -14,14 +13,12 @@ Builder.load_file("./widgets/create_audit_page.kv")
 
 class CreateAuditPage(FloatLayout):
 
-    counter = 1
-    question_list = ObjectProperty(None)
-    rv_data = []
+    counter = 0
+    question_list = ObjectProperty()
 
     def add_question(self):
-        self.rv_data[self.counter] = {"text": "test"}
-        self.question_list.add_widget(Button(id=str(self.counter), text="New Question", size_hint=(1, .25)))
         self.counter += 1
+        self.question_list.add_widget(Button(id=str(self.counter), text="New Question " + str(self.counter), size_hint=(1, 1)))
         print(self.counter)
 
 
