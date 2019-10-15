6219cf5b5b6f5d0b609eb573cb8f488366484634
bakolisty <bakolisty@gmail.com>
2019-10-15 16:26:55 -0700

    Changed question buttons to text boxs and made submit button print text box text

--- source/create_audit_page.py
+++ source/create_audit_page.py
@@ -4,6 +4,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.properties import ObjectProperty
 
 kivy.require("1.11.1")
@@ -12,16 +13,23 @@ Builder.load_file("./widgets/create_audit_page.kv")
 
 
 class CreateAuditPage(FloatLayout):
-
     counter = 0
-    question_list = ObjectProperty()
+    stack_list = ObjectProperty()
+    audit_title = ObjectProperty()
+    question_list = {}
 
     def add_question(self):
         self.counter += 1
-        self.question_list.add_widget(Button(id=str(self.counter), text="New Question " + str(self.counter),
-                                             size_hint=(1, None), height=100))
-        self.question_list.height = self.counter * 100
+        q_temp = TextInput(text="New Question " + str(self.counter), size_hint=(1, None), height=100)
+        self.stack_list.add_widget(q_temp)
+        self.question_list[str(self.counter)] = q_temp
+        self.stack_list.height += 100
         print(self.counter)
+
+    def submit_audit(self):
+        print(self.audit_title.text)
+        for i in range(1, self.counter):
+            print(self.question_list[str(i)].text)
 
 
 class TestApp(App):
