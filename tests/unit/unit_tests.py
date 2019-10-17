79ed986139e490e4e0a064d72d7ea13bdf8585fa
Erik Nordin <erik@pop-os.localdomain>
2019-10-16 18:26:49 -0700

    Add Unit Tests For Response

--- tests/unit/unit_tests.py
+++ tests/unit/unit_tests.py
@@ -1,4 +1,5 @@
 from test_severity import SeverityTests
+from test_response import ResponseTests
 from test_question import QuestionTests
 from test_audit_template import AuditTemplateTests
 from test_audit_template_builder import AuditTemplateBuilderTests
@@ -7,6 +8,7 @@ from test_audit_template_builder import AuditTemplateBuilderTests
 
 if __name__ == '__main__':
     SeverityTests.run_tests()
+    ResponseTests.run_tests()
     QuestionTests.run_tests()
     AuditTemplateTests.run_tests()
     AuditTemplateBuilderTests.run_tests()
