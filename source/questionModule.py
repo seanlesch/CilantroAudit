b408fe80c2b8345e69181fcc0904289b4403fbcf
aaron <dawson22@pdx.edu>
2019-10-18 12:49:09 -0700

    cleaned up code for sprint 1 merge
    Signed-off-by: aaron <dawson22@pdx.edu>

--- source/questionModule.py
+++ source/questionModule.py
@@ -1,22 +1,11 @@
 import kivy
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
 from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.properties import ObjectProperty
 
+Builder.load_file("./widgets/questionModule.kv")
 
 class QuestionModule(Widget):
-    ##variables with Object properties so that we can "see" whats going on in kv file
-    question_text = ObjectProperty(None)
-    delete_question = ObjectProperty(None)
-    yes_button = ObjectProperty(None)
-    no_button = ObjectProperty(None)
-    yes_btn_severity = ObjectProperty(None)
-    label = ObjectProperty(None)
 
     yes_val = 0
     no_val = 0
