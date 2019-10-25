6987651f7fd2a6a2542c3e72af57b278da89058b
Steven Tran <transtev@pdx.edu>
2019-10-25 15:34:52 -0700

    starting progress on gui

--- cilantro_audit/completed_audits_list_page.py
+++ cilantro_audit/completed_audits_list_page.py
@@ -1,24 +1,35 @@
+import kivy
+from kivy.lang import Builder
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.uix.scrollview import ScrollView
-from kivy.core.window import Window
 from kivy.uix.label import Label
-from kivy.app import runTouchApp
+from kivy.app import runTouchApp, App
 
-rooter = GridLayout(cols=2)
-layout = GridLayout(cols=1, spacing=10, size_hint_y=None)
-# Make sure the height is such that there is something to scroll.
-layout.bind(minimum_height=layout.setter('height'))
-troast = GridLayout(rows=2)
+from constants import KIVY_REQUIRED_VERSION
+
+kivy.require(KIVY_REQUIRED_VERSION)
+
+kvfile = Builder.load_file('./widgets/completed_audits_list_page.kv')
+
+
+class CompletedAuditsListPage(Screen):
+    def build(self):
+        return kvfile
+
+
+"""
 for i in range(100):
     btn = Button(text=str(i), size_hint_y=None, height=40)
-    layout.add_widget(btn)
-root = ScrollView()
-root.add_widget(layout)
-troast.add_widget(Label(text="Completed Audits", size_hint_y=0.1))
-troast.add_widget(root)
-rooter.add_widget(GridLayout(cols=1))
-rooter.add_widget(troast)
+    audit_list.add_widget(btn)
+"""
 
-runTouchApp(rooter)
 
+class TestApp(App):
+    def build(self):
+        return CompletedAuditsListPage()
+
+
+TestApp().run()
