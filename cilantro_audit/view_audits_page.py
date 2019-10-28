1d328bf45d6ca06a3432741e5d3c8422cdabd45d
BaderAlshaya <alshayabader@gmail.com>
2019-10-28 07:09:46 -0700

    Incomplete. Modified the general layout for viewing the list of submitted audits.

--- cilantro_audit/view_audits_page.py
+++ cilantro_audit/view_audits_page.py
@@ -1,14 +1,16 @@
 import kivy
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
-from kivy.uix.screenmanager import ScreenManager
 
 # Required version
 kivy.require('1.11.1')
 
 
 class ThisPage(Screen):
-    pass
+    def get_audits(self):
+        for i in range(0, 10):
+            self.name.audits_list.add_widget(Button(text=i))
 
 
 class AnotherPage(Screen):
@@ -22,10 +24,7 @@ class ViewSubmittedAudits(App):
     def build(self):
         self.title = 'CilantroAudit - Submitted Audits'
         self.load_kv('./widgets/view_audits_page.kv')
-        sm = ScreenManager()
-        sm.add_widget(self.root)
-        sm.add_widget(AnotherPage())
-        return sm
+        return self.root
 
 
 if __name__ == '__main__':
