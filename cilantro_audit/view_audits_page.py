8b17946b23c08bdf01cc84031c967f16f93fd0d8
BaderAlshaya <alshayabader@gmail.com>
2019-10-28 19:02:32 -0700

    Added dynamic adding of buttons.

--- cilantro_audit/view_audits_page.py
+++ cilantro_audit/view_audits_page.py
@@ -1,5 +1,6 @@
 import kivy
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.uix.screenmanager import ScreenManager
@@ -8,10 +9,20 @@ from kivy.uix.screenmanager import ScreenManager
 kivy.require('1.11.1')
 
 
-class ThisPage(Screen):
-    def get_audits(self):
+class Manager(ScreenManager):
+    pass
+
+
+class HomePage(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Clock.schedule_once(self.callback)
+
+    def callback(self, *args):
         for i in range(0, 10):
-            self.name.audits_list.add_widget(Button(text=i))
+            btn = Button(text=str(i + 1),
+                         size_hint_y=None)
+            self.ids["audits_list"].add_widget(btn)
 
 
 class AnotherPage(Screen):
@@ -25,10 +36,7 @@ class ViewSubmittedAudits(App):
     def build(self):
         self.title = 'CilantroAudit - Submitted Audits'
         self.load_kv('./widgets/view_audits_page.kv')
-        sm = ScreenManager()
-        sm.add_widget(self.root)
-        sm.add_widget(AnotherPage())
-        return sm
+        return self.root
 
 
 if __name__ == '__main__':
