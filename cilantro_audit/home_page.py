874d89412ae5a70c58d8c2a17c85a425eeda4aeb
Erik Nordin <aeketn@gmail.com>
2019-10-29 17:07:39 -0700

    Strange Behavior with home page

--- cilantro_audit/home_page.py
+++ cilantro_audit/home_page.py
@@ -6,6 +6,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.screenmanager import ScreenManager
 
 from cilantro_audit.auditor_page import AuditorPage
+from cilantro_audit.completed_audits_list_page import CompletedAuditsListPage
 from cilantro_audit.constants import KIVY_REQUIRED_VERSION, ADMIN_SCREEN, HOME_SCREEN, AUDITOR_SCREEN, CREATE_AUDIT_PAGE
 from cilantro_audit.create_audit_page import CreateAuditPage
 
@@ -16,7 +17,6 @@ Builder.load_file('./widgets/admin_page.kv')
 
 # Create the screen manager
 sm = ScreenManager()
-
 
 class HomePage(Screen):
     pass
@@ -42,7 +42,7 @@ class CilantroAudit(App):
         sm.add_widget(AdminPage(name=ADMIN_SCREEN))
         sm.add_widget(AuditorPage(name=AUDITOR_SCREEN))
         sm.add_widget(CreateAuditPage(name=CREATE_AUDIT_PAGE))
-        sm.add_widget(CreateAuditPage(name="CompletedAuditsListPage"))
+        #sm.add_widget(CompletedAuditsListPage(name="CompletedAuditsListPage"))
 
         self.title = 'CilantroAudit'
         return sm
