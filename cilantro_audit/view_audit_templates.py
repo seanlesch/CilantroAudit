b64a849be6f79d5ce939ef97c5feab57622ac8e7
bradlet <bradlet2@pdx.edu>
2019-10-29 19:11:55 -0700

    Connect to database
    Currently hooks up to a testdb and prints the titles of all
    auditTemplate objects in the db.

--- cilantro_audit/view_audit_templates.py
+++ cilantro_audit/view_audit_templates.py
@@ -4,10 +4,13 @@ from kivy.clock import Clock
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.uix.screenmanager import ScreenManager
+from audit_template import AuditTemplate
+from mongoengine import connect
 
 # Required version
 kivy.require('1.11.1')
 
+connect("toost")
 
 class Manager(ScreenManager):
     pass
@@ -19,8 +22,9 @@ class RootPage(Screen):
         Clock.schedule_once(self.callback)
 
     def callback(self, *args):
-        for i in range(1, 10):
-            btn = AuditButton(text="Audit " + str(i))
+        titles = list(map(lambda template: template.title, AuditTemplate.objects().only('title')))
+        for title in titles:
+            btn = AuditButton(text=title)
             self.ids["audits_list"].add_widget(btn)
 
     # def get_audit_templates(self):
