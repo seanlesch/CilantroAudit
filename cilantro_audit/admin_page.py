dff4d995205a4229289e750424851c3a70dfc429
Erik Nordin <aeketn@gmail.com>
2019-10-30 20:19:19 -0700

    Remove unnecessary paramteres in admin page

--- cilantro_audit/admin_page.py
+++ cilantro_audit/admin_page.py
@@ -6,6 +6,5 @@ from cilantro_audit.constants import COMPLETED_AUDITS_LIST_PAGE
 class AdminPage(Screen):
     pass
 
-    def load_completed_audits_list_page(self, root, widget):
+    def load_completed_audits_list_page(self):
         root.manager.current = COMPLETED_AUDITS_LIST_PAGE
-        widget.load_completed_audits()
