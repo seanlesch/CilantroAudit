3da2b971c6b16081f53be30abc46b0d15944da68
Sean Lesch <seadile@gmail.com>
2019-11-01 20:22:52 -0700

    Made ViewAuditTemplates screen accessible from Homepage

--- cilantro_audit/home_page.py
+++ cilantro_audit/home_page.py
@@ -6,9 +6,10 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.screenmanager import ScreenManager
 
 from cilantro_audit.auditor_page import AuditorPage
-from cilantro_audit.constants import KIVY_REQUIRED_VERSION, ADMIN_SCREEN, HOME_SCREEN, AUDITOR_SCREEN,\
-    CREATE_AUDIT_TEMPLATE_PAGE
+from cilantro_audit.constants import KIVY_REQUIRED_VERSION, ADMIN_SCREEN, HOME_SCREEN, AUDITOR_SCREEN, \
+    CREATE_AUDIT_TEMPLATE_PAGE, VIEW_AUDIT_TEMPLATES
 from cilantro_audit.create_audit_template_page import CreateAuditTemplatePage
+from view_audit_templates import ViewAuditTemplates
 
 kivy.require(KIVY_REQUIRED_VERSION)
 
@@ -43,6 +44,7 @@ class CilantroAudit(App):
         sm.add_widget(AdminPage(name=ADMIN_SCREEN))
         sm.add_widget(AuditorPage(name=AUDITOR_SCREEN))
         sm.add_widget(CreateAuditTemplatePage(name=CREATE_AUDIT_TEMPLATE_PAGE))
+        sm.add_widget(ViewAuditTemplates(name=VIEW_AUDIT_TEMPLATES))
 
         self.title = 'CilantroAudit'
         return sm
--- cilantro_audit/view_audit_templates.py
+++ cilantro_audit/view_audit_templates.py
@@ -3,52 +3,36 @@ from kivy.app import App
 from mongoengine import connect
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
-from kivy.uix.screenmanager import ScreenManager
+from kivy.lang import Builder
 
-from cilantro_audit.constants import KIVY_REQUIRED_VERSION, HOME_SCREEN, AUDITOR_SCREEN
+from cilantro_audit.constants import KIVY_REQUIRED_VERSION
 from cilantro_audit.constants import PROD_DB
-from cilantro_audit.home_page import HomePage
-from cilantro_audit.auditor_page import AuditorPage
 from cilantro_audit.audit_template import AuditTemplate
 
 # Required Version
 kivy.require(KIVY_REQUIRED_VERSION)
 
-# Default Database Connection
-connect(PROD_DB)
 
-
-class Manager(ScreenManager):
-    pass
-
-
-class ThisPage(Screen):
-    def get_audit_templates(self):
-        titles = list(map(lambda template: template.title, AuditTemplate.objects().only('title')))
-        for title in titles:
-            self.ids["audits_list"].add_widget(AuditButton(text=title))
+Builder.load_file("./widgets/view_audit_templates.kv")
 
 
 class AuditButton(Button):
     pass
 
 
-class ViewAuditTemplates(App):
+class ViewAuditTemplates(Screen):
+    connect(PROD_DB)
+
+    def get_audit_templates(self):
+        titles = list(map(lambda template: template.title, AuditTemplate.objects().only('title')))
+        for title in titles:
+            self.ids["audits_list"].add_widget(AuditButton(text=title))
+
+
+class TestApp(App):
     def build(self):
-        self.title = 'CilantroAudit - Audit Templates'
-        self.load_kv('./widgets/view_audit_templates.kv')
-
-        # Initialize this page and set the data
-        root_page = ThisPage()
-        root_page.get_audit_templates()
-
-        # Add all associated pages to the root manager
-        self.root.add_widget(root_page)
-        self.root.add_widget(HomePage(name=HOME_SCREEN))
-        self.root.add_widget(AuditorPage(name=AUDITOR_SCREEN))
-
-        return self.root
+        return ViewAuditTemplates()
 
 
 if __name__ == '__main__':
-    ViewAuditTemplates().run()
+    TestApp.run()
