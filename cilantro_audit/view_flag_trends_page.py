e33da49b3b9fa7a46a4a9b3cfcfa45848d4e1f6a
Bader Alshaya <bader@Baders-MacBook-Pro.local>
2019-11-08 00:01:44 -0800

    Merged ticket-92 and created the general page layout for viewing all flagged questions.

--- cilantro_audit/view_flag_trends_page.py
+++ cilantro_audit/view_flag_trends_page.py
@@ -1,10 +1,22 @@
-import kivy
-
-from kivy.uix.screenmanager import Screen
+from kivy import require
+from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
 
+from cilantro_audit.constants import KIVY_REQUIRED_VERSION
+
+require(KIVY_REQUIRED_VERSION)
 Builder.load_file("./widgets/view_flag_trends_page.kv")
 
 
 class ViewFlagTrendsPage(Screen):
     pass
+
+
+class TestApp(App):
+    def build(self):
+        return ViewFlagTrendsPage()
+
+
+if __name__ == '__main__':
+    TestApp().run()
