0853165b4b837fb8fa7a91672b7470a292a94b91
bakolisty <bakolisty@gmail.com>
2019-11-08 14:30:34 -0800

    Adding new layout for flag trends page.

--- cilantro_audit/view_flag_trends_page.py
+++ cilantro_audit/view_flag_trends_page.py
@@ -4,6 +4,7 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
+from kivy.uix.label import Label
 
 from mongoengine import connect
 
@@ -18,15 +19,33 @@ connect(PROD_DB)
 
 class ViewFlagTrendsPage(Screen):
     questions_list = ObjectProperty()
+    question_text_col = ObjectProperty()
+    audit_col = ObjectProperty()
+    times_flagged_col = ObjectProperty()
+    refresh_button = ObjectProperty()
 
     def __init__(self, **kw):
         super().__init__(**kw)
+        # self.question_text_col.bind(minimum_height=self.questions_list.setter("height"))
+        # self.audit_col.bind(minimum_height=self.questions_list.setter("height"))
+        # self.times_flagged_col.bind(minimum_height=self.questions_list.setter("height"))
+        self.audits = []
         self.get_flagged_questions()
 
     def get_flagged_questions(self):
-        for completed_audit in CompletedAudit.objects.all():
-            for answer in completed_audit.answers:
-                self.questions_list.add_widget(QuestionButton(text=answer.text))
+        self.audits = list(CompletedAudit.objects())
+        self.refresh_flagged_questions()
+
+    def refresh_flagged_questions(self):
+        self.question_text_col.clear_widgets()
+        self.audit_col.clear_widgets()
+
+        for audit in self.audits:
+            for answers in audit.answers:
+                btn = Button(text=answers.text, size_hint_y=None, height=40)
+                self.question_text_col.add_widget(btn)
+                lbl = Label(text=audit.title, size_hint_y=None, height=40)
+                self.auditor_col.add_widget(lbl)
 
 
 class QuestionButton(Button):
