0231115e97057b3a33074a63a984175ec2acab9f
Bader Alshaya <bader@Baders-MacBook-Pro.local>
2019-11-08 00:09:42 -0800

    Created a page_layout initial design (incomplete).

---
+++ cilantro_audit/templates/page_layout.py
@@ -0,0 +1,23 @@
+from os import path
+from kivy import require
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+
+from cilantro_audit.constants import KIVY_REQUIRED_VERSION
+
+require(KIVY_REQUIRED_VERSION)
+
+
+class PageLayout(Screen):
+    Builder.load_file(path.dirname(path.abspath(__file__)) + "/widgets/page_layout.kv")
+    pass
+
+
+class TestApp(App):
+    def build(self):
+        return PageLayout()
+
+
+if __name__ == '__main__':
+    TestApp().run()
