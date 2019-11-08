c32d081bd626c041764ac42f763b8e26708151d3
Kegan Bako-Listy <bakolisty@gmail.com>
2019-11-08 15:36:29 -0800

    Updated question display and layout.

--- cilantro_audit/view_flag_trends_page.py
+++ cilantro_audit/view_flag_trends_page.py
@@ -11,6 +11,7 @@ from mongoengine import connect
 from cilantro_audit.constants import KIVY_REQUIRED_VERSION
 from cilantro_audit.constants import PROD_DB
 from cilantro_audit.completed_audit import CompletedAudit
+from cilantro_audit.audit_template import Severity
 
 require(KIVY_REQUIRED_VERSION)
 Builder.load_file("./widgets/view_flag_trends_page.kv")
@@ -39,13 +40,28 @@ class ViewFlagTrendsPage(Screen):
     def refresh_flagged_questions(self):
         self.question_text_col.clear_widgets()
         self.audit_col.clear_widgets()
+        self.times_flagged_col.clear_widgets()
+        answer_list = []
 
         for audit in self.audits:
-            for answers in audit.answers:
-                btn = Button(text=answers.text, size_hint_y=None, height=40)
-                self.question_text_col.add_widget(btn)
-                lbl = Label(text=audit.title, size_hint_y=None, height=40)
-                self.auditor_col.add_widget(lbl)
+            for answer in audit.answers:
+                if answer.severity == Severity.red():
+                    match_flag = False
+                    for ans in answer_list:
+                        if ans[1] == audit.title and ans[0] == answer.text:
+                            ans[2] += 1
+                            match_flag = True
+                            break
+                    if not match_flag:
+                        answer_list.append([answer.text, audit.title, 1])
+
+        for answer in answer_list:
+            btn = Button(text=answer[0], size_hint_y=None, height=40)
+            self.question_text_col.add_widget(btn)
+            lbl = Label(text=answer[1], size_hint_y=None, height=40)
+            self.audit_col.add_widget(lbl)
+            lbl = Label(text=str(answer[2]), size_hint_y=None, height=40)
+            self.times_flagged_col.add_widget(lbl)
 
 
 class QuestionButton(Button):
