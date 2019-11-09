f418a8ba622585679cecbc71a0cad9a42f560605
Bader Alshaya <bader@Baders-MacBook-Pro.local>
2019-11-08 16:27:58 -0800

    Reformatting.

--- cilantro_audit/home_page.py
+++ cilantro_audit/home_page.py
@@ -8,6 +8,7 @@ from kivy.uix.screenmanager import ScreenManager
 from cilantro_audit.admin_page import AdminPage
 from cilantro_audit.auditor_page import AuditorPage
 from cilantro_audit.create_audit_template_page import CreateAuditTemplatePage
+from cilantro_audit.create_completed_audit_page import CreateCompletedAuditPage
 from cilantro_audit.completed_audits_list_page import CompletedAuditsListPage
 from cilantro_audit.auditor_completed_audits_list_page import AuditorCompletedAuditsListPage
 from cilantro_audit.view_audit_templates import ViewAuditTemplates
@@ -16,7 +17,6 @@ from cilantro_audit.view_flag_trends_page import ViewFlagTrendsPage
 from cilantro_audit.constants import KIVY_REQUIRED_VERSION, ADMIN_SCREEN, HOME_SCREEN, AUDITOR_SCREEN, \
     CREATE_AUDIT_TEMPLATE_PAGE, COMPLETED_AUDITS_LIST_PAGE, AUDITOR_COMPLETED_AUDITS_LIST_PAGE, VIEW_AUDIT_TEMPLATES, \
     CREATE_COMPLETED_AUDIT_PAGE, VIEW_FLAG_TRENDS_PAGE
-from cilantro_audit.create_completed_audit_page import CreateCompletedAuditPage
 
 
 kivy.require(KIVY_REQUIRED_VERSION)
