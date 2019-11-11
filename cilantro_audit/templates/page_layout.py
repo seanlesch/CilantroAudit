b3049f6c45ececbafc2d8557e1371d954f480d46
Bader Alshaya <bader@Baders-MacBook-Pro.local>
2019-11-11 11:58:14 -0800

    More changes.

--- cilantro_audit/templates/page_layout.py
+++ cilantro_audit/templates/page_layout.py
@@ -4,19 +4,21 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 
+from cilantro_audit.templates.audit_button import AuditButtonTemplate
+
 from cilantro_audit.constants import KIVY_REQUIRED_VERSION
 
 require(KIVY_REQUIRED_VERSION)
 
 
-class PageLayout(Screen):
+class PageTemplate(Screen):
     Builder.load_file(path.dirname(path.abspath(__file__)) + "/widgets/page_layout.kv")
     pass
 
 
 class TestApp(App):
     def build(self):
-        return PageLayout()
+        return PageTemplate()
 
 
 if __name__ == '__main__':
