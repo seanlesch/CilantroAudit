071d9230f2123a1df5376d3f713149890dfc518c
Steven Tran <transtev@pdx.edu>
2019-11-11 19:42:56 -0800

    cleaning up code

--- cilantro_audit/create_completed_audit_page.py
+++ cilantro_audit/create_completed_audit_page.py
@@ -2,7 +2,6 @@ import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty, StringProperty
-from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen
