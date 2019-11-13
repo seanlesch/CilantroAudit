144d391a142b319a9310a95da862f192e60645fb
bakolisty <bakolisty@gmail.com>
2019-11-13 08:46:11 -0800

    Adding unique entries as a member of the view flag trends page so it can be accessed by different methods.

--- cilantro_audit/view_flag_trends_page.py
+++ cilantro_audit/view_flag_trends_page.py
@@ -22,27 +22,26 @@ class ViewFlagTrendsPage(Screen):
     audit_title_col = ObjectProperty()
     question_text_col = ObjectProperty()
     times_flagged_col = ObjectProperty()
+    unique_entries = []
 
     def __init__(self, **kw):
         super().__init__(**kw)
         self.get_flagged_questions()
 
     def get_flagged_questions(self):
-        unique_entries = []
-
         for audit in list(CompletedAudit.objects()):
             for answer in audit.answers:
                 if answer.severity == Severity.red():
                     is_unique = True
-                    for entry in unique_entries:
+                    for entry in self.unique_entries:
                         if entry[0] == audit.title:
                             entry[2] += 1
                             is_unique = False
                             break
                     if is_unique:
-                        unique_entries.append([audit.title, answer.text, 1])
+                        self.unique_entries.append([audit.title, answer.text, 1])
 
-        for entry in unique_entries:
+        for entry in self.unique_entries:
             self.audit_title_col.add_widget(EntryLabel(text=entry[0]))
             self.question_text_col.add_widget(EntryLabel(text=entry[1]))
             self.times_flagged_col.add_widget(EntryLabel(text=str(entry[2])))
@@ -51,6 +50,7 @@ class ViewFlagTrendsPage(Screen):
         self.audit_title_col.clear_widgets()
         self.question_text_col.clear_widgets()
         self.times_flagged_col.clear_widgets()
+        self.unique_entries = []
         self.get_flagged_questions()
 
 
