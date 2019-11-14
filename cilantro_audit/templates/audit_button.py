23767d7d54c212f29add1fc8d7d6aef55024eee8
Bader Alshaya <bader@Baders-MacBook-Pro.local>
2019-11-14 04:35:39 -0800

    Version 1 widget templates completed.

--- cilantro_audit/templates/audit_button.py
+++ cilantro_audit/templates/audit_button.py
@@ -10,13 +10,13 @@ kivy.require(KIVY_REQUIRED_VERSION)
 Builder.load_file(os.path.dirname(os.path.abspath(__file__)) + "/widgets/audit_button.kv")
 
 
-class AuditButtonTemplate(Button):
+class AuditButton(Button):
     pass
 
 
 class RunApp(App):
     def build(self):
-        return AuditButtonTemplate()
+        return AuditButton()
 
 
 if __name__ == '__main__':
--- cilantro_audit/templates/audit_label.py
+++ cilantro_audit/templates/audit_label.py
@@ -11,13 +11,13 @@ kivy.require(KIVY_REQUIRED_VERSION)
 Builder.load_file(os.path.dirname(os.path.abspath(__file__)) + "/widgets/audit_label.kv")
 
 
-class AuditLabelTemplate(Label):
+class AuditLabel(Label):
     pass
 
 
 class RunApp(App):
     def build(self):
-        return AuditLabelTemplate()
+        return AuditLabel()
 
 
 if __name__ == '__main__':
