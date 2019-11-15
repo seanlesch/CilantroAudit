2f29fe8f9c2f2c6fb0cb2d9e465cd1c0544f6e9d
Bader Alshaya <bader@Baders-MacBook-Pro.local>
2019-11-14 20:03:57 -0800

    Renamed the template widgets to Cilantro instead of Audit.

--- cilantro_audit/templates/cilantro_page.py
+++ cilantro_audit/templates/cilantro_page.py
@@ -5,16 +5,16 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 
-from cilantro_audit.templates.audit_label import AuditLabel
-from cilantro_audit.templates.audit_button import AuditButton
+from cilantro_audit.templates.cilantro_label import CilantroLabel
+from cilantro_audit.templates.cilantro_button import CilantroButton
 
 from cilantro_audit.constants import KIVY_REQUIRED_VERSION
 
 require(KIVY_REQUIRED_VERSION)
-Builder.load_file(path.dirname(path.abspath(__file__)) + "/widgets/audit_page.kv")
+Builder.load_file(path.dirname(path.abspath(__file__)) + "/widgets/cilantro_page.kv")
 
 
-class AuditPage(Screen):
+class CilantroPage(Screen):
     page = ObjectProperty()
     body = ObjectProperty()
     header = ObjectProperty()
@@ -24,7 +24,7 @@ class AuditPage(Screen):
 
 class TestApp(App):
     def build(self):
-        return AuditPage()
+        return CilantroPage()
 
 
 if __name__ == '__main__':
