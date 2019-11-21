4b3ece595b1110d91bf336908b73127bd04363e9
Bader Alshaya <bader@Baders-MacBook-Pro.local>
2019-11-21 07:52:23 -0800

    Polishing.

--- cilantro_audit/view_audit_templates.py
+++ cilantro_audit/view_audit_templates.py
@@ -2,7 +2,6 @@ from kivy import require
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
-from kivy.uix.button import Button
 from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 
