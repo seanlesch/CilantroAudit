9cecb35d6b8260a9f9dfd6383ca61d15d856dfac
Bader Alshaya <alshayabader@gmail.com>
2019-11-23 08:37:57 -0800

    More Polishing.

--- cilantro_audit/admin_page.py
+++ cilantro_audit/admin_page.py
@@ -9,10 +9,7 @@ class TemplatesUnlockedPop(Popup):
 
 
 class AdminPage(Screen):
-
-    # Clears all audit template locks be setting their locked attribute to False
     def clear_all_locks(self):
         show = TemplatesUnlockedPop()
-
         AuditTemplate.objects().update(upsert=False, multi=True, locked=False)
         show.open()
