bf4b4b749614d98643f0710da054be89bc60d918
Erik Nordin <aeketn@gmail.com>
2019-12-03 20:40:24 -0800

    Add popup

--- cilantro_audit/admin_page.py
+++ cilantro_audit/admin_page.py
@@ -9,6 +9,25 @@ class AdminPage(Screen):
         AuditTemplate.objects().update(upsert=False, multi=True, locked=False)
         TemplatesUnlockedPop().open()
 
+    def open_reset_password_popup(self):
+        ResetPasswordPopup().open()
+
+
+class ResetPasswordPopup(Popup):
+    def on_open(self, *args):
+        super().on_open(*args)
+        if self:
+            self.content.children[1].focus = True
+
+    def current_password_is_valid(self, value):
+        if value == '12345':
+            return True
+        return False
+
+    def validate_password(self, value):
+        if value == '12345':
+            self.dismiss()
+            globals.screen_manager.current = globals.ADMIN_SCREEN
 
 class TemplatesUnlockedPop(Popup):
     pass
